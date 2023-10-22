13c25fd6fb3091fa15c5ebb07973e6261b7b2fb5
DavidLepe31 <d.alconerolepe@gmail.com>
2023-10-22 14:27:28 -0600

    ejercicio 1

--- PAT_HojaDeTrabajo02/Ejercicio01.cpp
+++ PAT_HojaDeTrabajo02/Ejercicio01.cpp
@@ -2,5 +2,28 @@
 
 Node<char>* Ejercicio01::rotateRight(Node<char>* head, int k)
 {
-    return nullptr;
+    if (head == nullptr) {
+        return head;
+    }
+
+    if (head->next == nullptr) {
+        return head;
+    }
+    Node<char>* tmp = head;
+    Node<char>* tmp2 = nullptr;
+    int banse = 0;
+
+    while (banse < k) {
+        tmp = head;
+        while (tmp && tmp->next) {
+            tmp2 = tmp;
+            tmp = tmp->next;
+
+        }
+        tmp->next = head;
+        head = tmp;
+        tmp2->next = nullptr;
+        banse++;
+    }
+    return head;
 }
