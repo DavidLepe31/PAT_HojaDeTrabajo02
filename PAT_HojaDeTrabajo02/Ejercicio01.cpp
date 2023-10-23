ca8523e0ced04357e172cb0002ba3056753e3359
DavidLepe31 <d.alconerolepe@gmail.com>
2023-10-23 07:56:34 -0600

    correccion 3

--- PAT_HojaDeTrabajo02/Ejercicio01.cpp
+++ PAT_HojaDeTrabajo02/Ejercicio01.cpp
@@ -13,32 +13,32 @@ int Ejercicio01::getlength(Node<char>* head1)
 
 Node<char>* Ejercicio01::rotateRight(Node<char>* head, int k)
 {
-    if (head == nullptr) {
-        return nullptr;
-    }
+    //if (head == nullptr) {
+    //    return nullptr;
+    //}
 
-    if (head->next == nullptr) {
-        return head;
-    }
-    Node<char>* tmp = head;
-    Node<char>* tmp2 = nullptr;
-    Node<char>* headf=head;
+    //if (head->next == nullptr) {
+    //    return head;
+    //}
+    //Node<char>* tmp = head;
+    //Node<char>* tmp2 = nullptr;
+    //Node<char>* headf=head;
 
-    int banse = 0;
-    int s = k % getlength(head);
-    while (banse < s) {
-        tmp = headf;
-       /* while (tmp != nullptr && tmp->next != nullptr) {
-            tmp2 = tmp;
-            tmp = tmp->next;
+    //int banse = 0;
+    //int s = k % getlength(head);
+    //while (banse < s) {
+    //    tmp = headf;
+    //   /* while (tmp != nullptr && tmp->next != nullptr) {
+    //        tmp2 = tmp;
+    //        tmp = tmp->next;
 
-        }*/
-    
-        tmp->next = headf;
-        headf = tmp;
-        tmp2->next = nullptr;
-        tmp2 = tmp2->next;
-        banse++;
-    }
+    //    }*/
+    //
+    //    tmp->next = headf;
+    //    headf = tmp;
+    //    tmp2->next = nullptr;
+    //    tmp2 = tmp2->next;
+    //    banse++;
+    //}
     return nullptr;
 }
