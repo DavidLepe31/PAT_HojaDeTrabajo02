2f66a9b86c97cd6efcabf0768f87a4f4b9ca82d4
DavidLepe31 <d.alconerolepe@gmail.com>
2023-10-23 16:21:48 -0600

    ejer2 terminado

--- PAT_HojaDeTrabajo02/Ejercicio02.cpp
+++ PAT_HojaDeTrabajo02/Ejercicio02.cpp
@@ -12,57 +12,115 @@ Node<int>* Ejercicio02::addTwoNumbers(Node<int>* l1, Node<int>* l2)
     if (l2 == nullptr) {
         return l1;
     }
-    if (l1->value == 0) {
+    if (l1->value == 0 && l1->next == nullptr) {
         return l2;
     }
-    if (l2->value == 0) {
+    if (l2->value == 0 && l2->next == nullptr) {
         return l1;
     }
-    Node<int>* headf = nullptr;
-    Node<int>* tmp=l1;
-    int num;
-    int c1 = 0;
-    int c2 = 0;
-    while (tmp != nullptr) {
-        tmp = tmp->next;
-        c1++;
-    }
-    c1 = c1 - 1;
-    c2 = c1 - 1;
-    num = l1->value;
-    l1 = l1->next;
-    while (l1 != nullptr) {
-        num += (l1->value) * (pow(10, (c1 - c2)));
+    Node<int>* tmp = nullptr;
+    Node<int>* tmp2 = nullptr;
+    int carry = 0;
+    Node<int>* headf = new Node<int>();
+    int sum = 0;
+    sum = l1->value + l2->value + carry;
+    if (sum >= 10) {
+        headf->value = (sum % 10);
+        headf->next = nullptr;
         l1 = l1->next;
-        c2--;
-    }
-    c1 = 0;
-    c2 = 0;
-    tmp = l2;
-    while (tmp != nullptr) {
-        tmp = tmp->next;
-        c1++;
-    }
-    c1 = c1 - 1;
-    c2 = c1 - 1;
-    num = l2->value;
-    l2 = l2->next;
-    while (l2 != nullptr) {
-        num += (l2->value) * (pow(10, (c1 - c2)));
         l2 = l2->next;
-        c2--;
+        carry = 1;
     }
-    std::string snum;
-    snum = std::to_string(num);
-    Node<int> tail = new Node<int>();
-    tail->value = stoi(snum[snum.size() - 1]);
-    tail->next = nullptr;
-    headf = tail;
-    for (int i = 1; i < (snum.size() - 1); i++) {
-        Node<int> newNode = new Node<int>();
-        newNode->value = stoi(snum[(snum.size()) - 1]);
-        newNode->next = headf;
-        headf = newNode;
+    else {
+        headf->valueue = sum;
+        headf->next = nullptr;
+        l1 = l1->next;
+        l2 = l2->next;
+        carry = 0;
+    }
+    tmp = headf;
+    while (l1 != nullptr && l2 != nullptr) {
+        Node<int>* newNode = new Node<int>();
+        int sum = 0;
+        sum = l1->value + l2->value + carry;
+        if (sum >= 10) {
+            newNode->value = (sum % 10);
+            newNode->next = nullptr;
+            tmp->next = newNode;
+            tmp = tmp->next;
+            l1 = l1->next;
+            l2 = l2->next;
+            carry = 1;
+        }
+        else {
+            newNode->value = sum;
+            newNode->next = nullptr;
+            tmp->next = newNode;
+            tmp = tmp->next;
+            l1 = l1->next;
+            l2 = l2->next;
+            carry = 0;
+        }
+
+    }
+    if (l1 != nullptr) {
+        while (l1 != nullptr) {
+            Node<int>* newNode = new Node<int>();
+            int sum = 0;
+            sum = l1->value + carry;
+            if (sum >= 10) {
+                newNode->value = (sum % 10);
+                newNode->next = nullptr;
+                tmp->next = newNode;
+                tmp = tmp->next;
+                l1 = l1->next;
+                carry = 1;
+                cout << "entre" << endl;
+            }
+            else {
+                newNode->value = sum;
+                newNode->next = nullptr;
+                tmp->next = newNode;
+                tmp = tmp->next;
+                l1 = l1->next;
+                carry = 0;
+            }
+
+        }
+    }
+    else {
+        while (l2 != nullptr) {
+            Node<int>* newNode = new Node<int>();
+            int sum = 0;
+            sum = l2->value + carry;
+            if (sum >= 10) {
+                newNode->value = (sum % 10);
+                newNode->next = nullptr;
+                tmp->next = newNode;
+                tmp = tmp->next;
+                l2 = l2->next;
+                carry = 1;
+                cout << "entre" << endl;
+            }
+            else {
+                newNode->value = sum;
+                newNode->next = nullptr;
+                tmp->next = newNode;
+                tmp = tmp->next;
+                l2 = l2->next;
+                carry = 0;
+            }
+
+        }
+
+    }
+
+    if (carry == 1) {
+        Node<int>* newNode = new Node<int>();
+        newNode->value = carry;
+        newNode->next = nullptr;
+        tmp->next = newNode;
+        tmp = tmp->next;
     }
     return headf;
     
